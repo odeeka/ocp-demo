e245b91502f2cb46d344f772394ccfd9594af853
Tibor Petróczy <ptibor@netbuild.hu>
2023-08-02 23:14:03 +0200

    Update cc-developers.go

--- cc-developers.go
+++ cc-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello Admin!"
+		response = "Hello Student!"
 	}
 
 	fmt.Fprintln(w, response)
